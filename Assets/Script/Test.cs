a7f1aaab141255e95314976b6233174acda2cb5a
HouJiXin <1518760556@qq.com>
2020-10-22 11:37:45 +0800

    ui设置改动，以及手柄添加

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,24 @@
+﻿using HTC.UnityPlugin.Vive;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ViveInput.GetPressDownEx(HandRole.RightHand, ControllerButton.Trigger))
+        {
+            Debug.Log("HandRole.RightHand");
+        }
+        //获取扳机轴
+        Debug.Log(ViveInput.GetAxisEx(HandRole.RightHand, ControllerAxis.Trigger));
+    }
+}
